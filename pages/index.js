2578c2272609735aa8158e0797c0b6018b38c186
sideshow682 <zoefilmz0@gmail.com>
2025-06-22 19:52:07 -0600

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: 30, textAlign: 'center' }}>
-      <h1>🎲 Bienvenue sur Kaspa Lottery</h1>
-      <p>Connexion au backend : <code>{process.env.NEXT_PUBLIC_BACKEND_URL}</code></p>
-      <p>Le frontend fonctionne !</p>
-    </div>
-  );
-}
