d8743987a3a7db9f0d2fb33a57db9e4c300924b2
sideshow682 <zoefilmz0@gmail.com>
2025-06-22 19:29:54 -0600

    Delete frontend directory

--- frontend/src/KaspaLotteryFront.jsx
+++
@@ -1,8 +0,0 @@
-// === Interface Web Professionnelle pour Kaspa Lottery ===
-// NOTE : Remplacé ici par version réduite (voir ton éditeur pour version complète)
-
-import { useEffect, useState } from 'react';
-
-export default function KaspaLotteryFront() {
-  return <div>🎲 Kaspa Lottery App (Code complet dans ton éditeur)</div>;
-}
