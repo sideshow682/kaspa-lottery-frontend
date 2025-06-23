90864bc3811b4447bbaaf01be7d1b1a1b809af86
sideshow682 <zoefilmz0@gmail.com>
2025-06-22 18:34:10 -0600

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KaspaLotteryFront from './KaspaLotteryFront';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KaspaLotteryFront />
+  </React.StrictMode>
+);
